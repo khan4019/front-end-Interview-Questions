b7963b41fbf22db665dee3756640e47432e26290
Md Khan <khan4019@gmail.com>
2014-01-03 22:15:07 -0600

    update button text and on click

--- js/toggleExample.js
+++ js/toggleExample.js
@@ -1,7 +1,11 @@
 (function () {
+	'use strict';
 	var allExamples = document.querySelectorAll('.toggleExample'),
+	classes,
 	toggleHandler = function (el) {
-		document.getElementById(this.id+'Example').classList.toggle('hide');
+		classes = document.getElementById(this.id+'Example').classList;		
+		this.innerHTML = (classes.contains('hide')) ? 'hide example' : 'show example';
+		classes.toggle('hide');
 	};
 
 	for (var i = allExamples.length - 1; i >= 0; i--) {
